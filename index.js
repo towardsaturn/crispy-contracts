080c3de904eccc34b94872eab67710c818a83fa8
Brian Le <minh100@gmail.com>
2022-02-19 18:46:40 -0800

    smart contract experimentations

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const { Client, PrivateKey, AccountCreateTransaction, AccountBalanceQuery, Hbar } = require("@hashgraph/sdk");
+const hedera = require("./hedera")
+const { Configuration, OpenAIApi } = require("openai");
+const { transcribeAudio } = require('./assemblyai');
+const app = express();
+const port = 3000;
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+app.get('/', async(req, res) => {
+    // hederaClient = await hedera.getClient();
+    let audioData = await transcribeAudio("https://bit.ly/3yxKEIY");
+    res.send(audioData);
+});
+
+app.post('/getBalance', async(req, res) => {
+    hederaClient = await hedera.getClient();
+    const accountBalance = await new AccountBalanceQuery()
+        .setAccountId("0.0.30771440")
+        .execute(hederaClient);
+    res.send("The account balance is: " + accountBalance.hbars.toTinybars() + " tinybar.");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
 })
