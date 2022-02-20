de9da8e3fcaba23f9b032c0c6c89ee6f4738ef9a
Brian Le <minh100@gmail.com>
2022-02-20 04:36:33 -0800

    update port

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 require("dotenv").config();
 
 // set the view engine to ejs
